837d42a601489879a83e01e1ed5d9d068365a711
조예설 <choyeseol07@gmail.com>
2024-09-13 15:43:36 +0900

    refactor: 코드야 깨끗해져라 얍

--- src/main/java/com/hash/harp/domain/plan/repository/PlanRepository.java
+++ src/main/java/com/hash/harp/domain/plan/repository/PlanRepository.java
@@ -1,9 +1,6 @@
 package com.hash.harp.domain.plan.repository;
 
-import com.hash.harp.domain.plan.domain.Header;
 import com.hash.harp.domain.plan.domain.Plan;
-import com.hash.harp.domain.plan.exception.HeaderNotFoundException;
-import com.hash.harp.domain.plan.exception.PlanNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
