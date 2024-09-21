2e4f7ec97ef5c10c1d5789fc677ab82971558fa6
조예설 <choyeseol07@gmail.com>
2024-09-21 15:18:01 +0900

    chore: 에러 로그 안뜨는거 고침

--- src/main/java/com/hash/harp/global/exception/ErrorResponse.java
+++ src/main/java/com/hash/harp/global/exception/ErrorResponse.java
@@ -2,13 +2,15 @@ package com.hash.harp.global.exception;
 
 public record ErrorResponse(
         int status,
-        String message
+        String message,
+        String code
 
 ) {
     @Override
     public String toString() {
         return "{\n" +
                 "\t\"status\": " + status +
+                ",\n\t\"code\": \"" + code + '\"' +
                 ",\n\t\"message\": \"" + message + '\"' +
                 "\n}";
     }
