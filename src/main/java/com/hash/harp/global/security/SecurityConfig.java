a108c148b8b149f496fedf4fadbc4a9809976ab8
조예설 <choyeseol07@gmail.com>
2024-10-11 10:38:02 +0900

    fix: cors 모든 도메인

--- src/main/java/com/hash/harp/global/security/SecurityConfig.java
+++ src/main/java/com/hash/harp/global/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(
                 List.of(
