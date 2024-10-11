549949adbb56685aa993866222ca8a1cdea9d0bd
조예설 <choyeseol07@gmail.com>
2024-10-11 11:12:57 +0900

    fix: cors 와일드카드 에러

--- src/main/java/com/hash/harp/global/security/SecurityConfig.java
+++ src/main/java/com/hash/harp/global/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(
                 List.of(
